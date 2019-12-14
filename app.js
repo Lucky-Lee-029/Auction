c9714d115072fc68bece4495d33c8a0d88a9818e
tvp-github <tavinhphuc.ctg@gmail.com>
2019-12-14 21:19:50 +0700

    + Error handling page

--- app.js
+++ app.js
@@ -3,28 +3,33 @@ const express = require('express');
 const bidders = require('./models/bidders.model');
 const categoryModel = require('./models/category.model')
 const exphbs = require('express-handlebars');
+const express_handlebars_sections = require('express-handlebars-sections');
 //Express instance
 const app = express();
 const PORT = process.env.PORT | 3000;
-
-app.use(express.static(__dirname + '/views/public'))
-    //Middleware
-app.use(async(req, res, next) => {
-    var data = await categoryModel.parentCategory();
-    var child = [];
-    for (parent of data) {
-        child.push(await categoryModel.childCategory(parent.id));
+//View engine
+app.engine('hbs', exphbs({
+    helpers: {
+        section: express_handlebars_sections()
     }
-    res.locals.cate = { parent: data, child }
-    next();
-})
-
-app.engine('hbs', exphbs());
+}));
 app.set('view engine', 'hbs');
-
-//User route
+//Middleware
+app.use(express.static(__dirname + '/views/public'))
+app.use(async(req, res, next) => {
+        var data = await categoryModel.parentCategory();
+        var child = [];
+        for (parent of data) {
+            child.push(await categoryModel.childCategory(parent.id));
+        }
+        res.locals.cate = { parent: data, child }
+        next();
+    })
+    //User route
 app.use('/', require('./routes/public/public.route'))
-
+app.use(function(err, req, res, next) {
+    res.render('errors')
+});
 //Listen at PORT
 app.listen(PORT, () => {
     console.log(`Listening Port: ${PORT}`);
