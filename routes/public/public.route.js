46a613409423ac47f36a4a49784c0b9fa46e70ec
congson291999 <1712726@student.hcmus.edu.vn>
2019-12-18 16:55:00 +0700

    test

--- routes/public/public.route.js
+++ routes/public/public.route.js
@@ -19,47 +19,5 @@ route.get('/category/:id', async(req, res) => {
     res.render('list_product', { data });;
 })
 
-route.get('/admin',(req, res)=>{
-    res.render('admin/action',{layout: 'admin'});
-})
 
-route.get('/admin/product/action',(req, res)=>{
-    res.render('admin/action', {layout: 'admin'});
-})
-route.get('/admin/product/pending',(req, res)=>{
-    res.render('admin/pending', {layout: 'admin'});
-})
-route.get('/admin/product/success',(req, res)=>{
-    res.render('admin/success', {layout: 'admin'});
-})
-route.get('/admin/product/fail',(req, res)=>{
-    res.render('admin/fail', {layout: 'admin'});
-})
-route.get('/admin/product/blocked',(req, res)=>{
-    res.render('admin/blocked', {layout: 'admin'});
-})
-route.get('/admin/user/bidder',(req, res)=>{
-    res.render('admin/bidder', {layout: 'admin'});
-})
-route.get('/admin/user/bidder',(req, res)=>{
-    res.render('admin/bidder', {layout: 'admin'});
-})
-route.get('/admin/user/seller',(req, res)=>{
-    res.render('admin/seller', {layout: 'admin'});
-})
-route.get('/admin/user/upgrade',(req, res)=>{
-    res.render('admin/upgrade-request', {layout: 'admin'});
-})
-route.get('/admin/user/bidder-blocked',(req, res)=>{
-    res.render('admin/bidder-blocked', {layout: 'admin'});
-})
-route.get('/admin/user/seller-blocked',(req, res)=>{
-    res.render('admin/seller-blocked', {layout: 'admin'});
-})
-route.get('/admin/category',(req, res)=>{
-    res.render('admin/category', {layout: 'admin'});
-})
-route.get('/admin/faqs',(req, res)=>{
-    res.render('admin/faqs', {layout: 'admin'});
-})
 module.exports = route;
