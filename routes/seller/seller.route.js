354b748c66dcacc78750c9211e37a1505d4dc22d
haile1999 <haile3107@gmail.com>
2019-12-20 12:14:41 +0700

    edit route and convert to hbs

--- routes/seller/seller.route.js
+++ routes/seller/seller.route.js
@@ -4,11 +4,14 @@ const sellerModel = require('../../models/seller.model')
 seller_route.get('/', (req, res) => {
         res.render('seller/dashboard',{layout: 'seller'});
 })
-seller_route.get('/product',(req,res)=>{
+seller_route.get('/product', (req, res) => {
     res.render('seller/product',{layout: 'seller'});
 })
 seller_route.get('/profile',(req,res)=>{
     res.render('seller/profile',{layout: 'seller'});
+})
+seller_route.get('/end', (req, res) => {
+    res.render('seller/product-ended',{layout: 'seller'});
 })
 // seller_route.get('/bidding',(req,res)=>{
 //     res.render('seller/product-bidding',{layout: 'seller'});
