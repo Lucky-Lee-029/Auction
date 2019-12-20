0dbe1318c38cff08280bb2b9e719e46326bc55fa
haile1999 <haile3107@gmail.com>
2019-12-20 08:48:41 +0700

    Edit route seller and convert view to hbs

--- routes/seller/seller.route.js
+++ routes/seller/seller.route.js
@@ -4,12 +4,12 @@ const sellerModel = require('../../models/seller.model')
 seller_route.get('/', (req, res) => {
         res.render('seller/dashboard',{layout: 'seller'});
 })
-// seller_route.get('/product',(req,res)=>{
-//     res.render('seller/product',{layout: 'false'});
-// })
-// seller_route.get('/feedback',(req,res)=>{
-//     res.render('seller/feedback', {layout: 'seller'});
-// })
+seller_route.get('/product',(req,res)=>{
+    res.render('seller/product',{layout: 'seller'});
+})
+seller_route.get('/profile',(req,res)=>{
+    res.render('seller/profile',{layout: 'seller'});
+})
 // seller_route.get('/bidding',(req,res)=>{
 //     res.render('seller/product-bidding',{layout: 'seller'});
 // })
