73af5e137f5fe5bdfa9d8680e90d638e10b05d08
haile1999 <haile3107@gmail.com>
2020-01-01 17:25:01 +0700

    seller view product

--- models/seller.model.js
+++ models/seller.model.js
@@ -2,26 +2,28 @@ const db = require('../utils/db');
 const tableName = 'seller';
 
 module.exports = {
-	all: () => db.load(`select * from ${tableName}`),
-	single: (id) => db.load(`select * from ${tableName} where id=${id}`),
+    all: () => db.load(`select * from ${tableName}`),
+    single: (id) => db.load(`select * from ${tableName} where id=${id}`),
 
-	insert(pro_id, id, name, price_start, price_end, step, auto_renew, description, duaration, created_at) {
-		return db.load(
-			`INSERT INTO products (id, seller_id ,name, price_start, price_end, buy_now, step, auto_renew, description, duration,status, created_at) VALUES
+    insert(pro_id, id, name, price_start, price_end, step, auto_renew, description, duaration, created_at) {
+        return db.load(
+            `INSERT INTO products (id, seller_id ,name, price_start, price_end, buy_now, step, auto_renew, description, duration,status, created_at) VALUES
             (${pro_id},${id},"${name}",${price_start},${price_end},${price_end},${step},${auto_renew},"${description}",${duaration},1,${created_at})`
-		);
-	},
+        );
+    },
 
-	maxId: () => db.load(`SELECT Max(id) as id From products`),
+    maxId: () => db.load(`SELECT Max(id) as id From products`),
 
-	del: (tableName, condition) => db.del(tableName, condition),
+    allActive: (id) => db.load(`select * from products where status=1 and seller_id=${id}`),
 
-	patch: (entity) => {
-		const condition = {
-			id: entity.id
-		};
-		delete entity.id;
-		// console.log(condition, entity);
-		return db.patch(tableName, entity, condition);
-	}
+    del: (tableName, condition) => db.del(tableName, condition),
+
+    patch: (entity) => {
+        const condition = {
+            id: entity.id
+        };
+        delete entity.id;
+        // console.log(condition, entity);
+        return db.patch(tableName, entity, condition);
+    }
 };
